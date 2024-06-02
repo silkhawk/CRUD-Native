ba14e28808ec70f3d2ed18d1be2e1de28d89d86d
Erwin.R <12win.r@gmail.com>
2024-06-02 20:56:47 +0700

    menambah file index.php dan formAdd.php

---
+++ formAdd.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>form Add</title>
+</head>
+<body>
+<form action="" method="">
+<table border="0" width="300">
+	<tr>
+		<th colspan="2">FORM TAMBAH</th>
+	</tr>
+	<tr>
+		<td>NIS</td>
+		<td><input type="" name="tNis"></td>
+	</tr>
+	<tr>
+		<td>NAMA</td>
+		<td><input type="" name="tNama"></td>
+	</tr>
+	<tr>
+		<td>ALAMAT</td>
+		<td><input type="" name="tAlamat"></td>
+	</tr>
+	<tr>
+		<td>&nbsp;</td>
+		<td><input type="submit" name="" value="SAVE"></td>
+	</tr>
+</table>
+</form>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>tugas</title>
+	<style type="text/css">
+		#a{
+			border: 1px solid red;
+			border-radius: 10px;
+		}
+		a{
+			text-decoration: none;
+			color: #0A6847;
+
+		}
+
+	</style>
+	<?php
+		include_once('koneksi.php');
+		$sql = "SELECT * FROM siswa";
+		$hasil = $conn->query($sql);
+	?>
+</head><body>
+<table border="0" width="400" id="a">
+	<tr>
+		<th>NO</th>
+		<th>NAMA</th>
+		<th>ALAMAT</th>
+		<th><a href="formAdd.php">TAMBAH DATA</a></th>
+	</tr>
+	<?php 	
+	while ($data = mysqli_fetch_array($hasil)) {	
+ 	?>
+	<tr>
+		<td><?php echo $data['nis']?></td>
+		<td><?php echo $data['nama']?></td>
+		<td><?php echo $data['alamat']?></td>
+	</tr>
+	<?php }?>
+	
+	
+</table>
+</body>
+</html>
